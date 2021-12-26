087d938aba97a68fb9f86e75e5e5de01f92c1be7
kindyluv <onyeukwuamara@gmail.com>
2021-12-26 16:13:37 +0100

    ...

---
+++ src/structuralPattern/ToyDisplay.java
@@ -0,0 +1,8 @@
+package structuralPattern;
+
+public class ToyDisplay implements Display{
+    @Override
+    public void display() {
+        System.out.println("Toys cant display on there own");
+    }
+}
