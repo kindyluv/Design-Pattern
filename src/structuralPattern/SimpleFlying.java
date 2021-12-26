9f183113c039527d70d2bcfbf724341f365c41f8
kindyluv <onyeukwuamara@gmail.com>
2021-12-26 14:21:01 +0100

    ...

---
+++ src/structuralPattern/SimpleFlying.java
@@ -0,0 +1,8 @@
+package structuralPattern;
+
+public class SimpleFlying implements IFlyBehaviour{
+    @Override
+    public void fly() {
+        System.out.println("I am Simply flying");
+    }
+}
